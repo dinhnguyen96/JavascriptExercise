73f746610a1af1ea7356b5700c526b9095a6b8d4
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2022-12-12 14:16:48 +0700

    add if_else_exercise file

---
+++ js/if_else_exercise.js
@@ -0,0 +1,14 @@
+function showInfo()
+{
+    let answer = prompt("What is the “official” name of JavaScript?");
+
+    if (answer === "ECMAScript")
+    {
+        alert("Right!");
+    }
+    else
+    {
+        alert("Didn’t know? ECMAScript!");
+    }
+}
+showInfo();
